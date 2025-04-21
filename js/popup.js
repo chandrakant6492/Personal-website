c4b7b22e73a8ad953f9a899692bf188a7d45f1c5
Chandrakant Singh <chandrakant6492@gmail.com>
2025-04-21 16:04:47 +0200

    popup update

---
+++ js/popup.js
@@ -0,0 +1,22 @@
+                    // when the window "load" event fires (i.e. preloader is done),
+                    // show our popup:
+                    window.addEventListener('load', () => {
+                        setTimeout(() => {
+                            document.querySelector('.welcome-modal__overlay').style.display = 'block';
+                            document.querySelector('.welcome-modal__body').style.display = 'block';
+                        }, 6000);
+                        });
+                  
+                    // a single function to hide both overlay & modal
+                    function hideModal() {
+                      document.querySelector('.welcome-modal__overlay').style.display = 'none';
+                      document.querySelector('.welcome-modal__body').style.display = 'none';
+                    }
+                  
+                    // close on clicking the × button
+                    document.querySelector('[data-ref="close"]')
+                      .addEventListener('click', hideModal);
+                  
+                    // close on clicking anywhere on the overlay
+                    document.querySelector('.welcome-modal__overlay')
+                      .addEventListener('click', hideModal);
