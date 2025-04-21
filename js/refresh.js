4390d087cf0e6edfe2a5f999f8a4a9fbf7dfbe0a
Chandrakant Singh <chandrakant6492@gmail.com>
2025-04-22 01:31:05 +0200

    add refresh.js

---
+++ js/refresh.js
@@ -0,0 +1,7 @@
+  // Check if this is the user's first visit after navigation
+  if (!sessionStorage.getItem('hasRefreshed')) {
+    setTimeout(function() {
+      sessionStorage.setItem('hasRefreshed', 'true');
+      location.reload();
+    }, 5000); // 5000 ms = 5 seconds
+  }
